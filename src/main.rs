f0a474fde61defdf11cef51e42444918e5624933
jiyuan0125 <jiyuan0125@126.com>
2022-12-14 01:26:19 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use clap::Parser;
+use dotenv::dotenv;
+use log::debug;
+
+mod chat;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(short, long)]
+    name: String,
+    #[arg(short, long)]
+    to_dial: Option<String>,
+    #[arg(short, long)]
+    listen_addr: Option<String>,
+}
+
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+    let cli = Cli::parse();
+    debug!("Cli: {:?}", cli);
+
+    chat::run(
+        &cli.name,
+        cli.listen_addr.as_deref(),
+        cli.to_dial.as_deref(),
+    )
+    .await?;
+
+    Ok(())
+}
